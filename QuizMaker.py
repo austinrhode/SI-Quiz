19897483998303bb8f67c0873f69b7e872e44b2c
Austin Rhode <ajr3021@g.rit.edu>
2021-02-22 09:21:45 -0500

    Updated files to be csv, not txt and removes cs1-sample as it is not relevant to this class

--- QuizMaker.py
+++ QuizMaker.py
@@ -4,6 +4,7 @@ from Question import *
 from TrueFalse import *
 from MultipleChoice import *
 from Vocab import *
+import csv
 
 
 
@@ -19,15 +20,14 @@ class QuizMaker:
         self.quiz = Quiz()
 
     def parseQuestion(self, line, number):
-        line = line.strip().split(',')
         if line[TYPE_POSITION] == 'T/F':
-            question = TrueFalse(line[ANSWER_POSITION].strip(), line[QUESTION_POSITION])
+            question = TrueFalse(line[ANSWER_POSITION].strip(), line[QUESTION_POSITION].strip().strip('"')) # i feel like I shouldn't need to do this, but I don't want it to print with quotes
             self.quiz.addQuestion(question)
         elif line[TYPE_POSITION] == 'V':
-            question = Vocab(line[ANSWER_POSITION].strip(), line[QUESTION_POSITION])
+            question = Vocab(line[ANSWER_POSITION].strip(), line[QUESTION_POSITION].strip().strip('"'))
             self.quiz.addQuestion(question)
         elif line[TYPE_POSITION] == 'M':
-            question = MultipleChoice(line[MULT_ANSWER_POSITION].strip(), line[QUESTION_POSITION])
+            question = MultipleChoice(line[MULT_ANSWER_POSITION].strip(), line[QUESTION_POSITION].strip().strip('"'))
             for option in line[QUESTION_POSITION + 1:MULT_ANSWER_POSITION]:
                 question.addOption(option)
             self.quiz.addQuestion(question)
@@ -38,7 +38,8 @@ class QuizMaker:
     def readFile(self, filename):
         line_count = 1
         try:
-            with open("Quiz Files/"+ filename) as file_:
+            with open("Quiz Files/"+ filename) as csv_file:
+                file_ = csv.reader(csv_file, delimiter=',', quotechar='"')
                 for line in file_:
                     if line != "\n":
                         self.parseQuestion(line, line_count)
